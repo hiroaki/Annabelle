7b396a7f1436fb2a20628ff0224c980941c23343
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-11-26 20:20:26 +0900

    kaminari default per page

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+dpp = ENV['KAMINARI_DEFAULT_PER_PAGE'].to_i
+dpp = dpp < 1 ? 10 : dpp
+
 Kaminari.configure do |config|
-  config.default_per_page = 10
+  config.default_per_page = dpp
   # config.max_per_page = nil
-  # config.window = 4
+  config.window = 3 # default = 4
   # config.outer_window = 0
   # config.left = 0
   # config.right = 0
