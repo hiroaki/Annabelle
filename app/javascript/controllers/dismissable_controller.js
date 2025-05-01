176ac423c5d33824f94f48a4e3647482282df028
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-05-01 14:12:53 +0900

    adjust text color based on element color

--- app/javascript/controllers/dismissable_controller.js
+++ app/javascript/controllers/dismissable_controller.js
@@ -1,12 +1,18 @@
 import { Controller } from '@hotwired/stimulus'
 
+/* このコントローラを接続した要素に、 "x" ボタンを右上に追加し、その要素を取り除くことができるようにします。
+  CSS に tailwind を利用していることが前提です。
+  */
 export default class extends Controller {
   connect() {
+    const matchResult = this.element.classList.toString().match(/text-(\w+)-\d+/);
+    const colorClass = matchResult ? matchResult[1] : 'gray';
+
     const closeButton = document.createElement('button');
     closeButton.innerHTML = '&times;';
     closeButton.setAttribute('type', 'button');
     closeButton.setAttribute('aria-label', 'Close');
-    closeButton.className = 'absolute top-2 right-2 text-blue-700 hover:text-blue-900';
+    closeButton.className = `absolute top-2 right-2 text-${colorClass}-700 hover:text-${colorClass}-900`;
     closeButton.addEventListener('click', () => this.dismiss());
 
     this.element.classList.add('relative');
