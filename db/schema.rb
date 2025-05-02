25cd68f97233f23ee1190b184f6311ccc74d25b2
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-05-02 19:43:24 +0900

    create table authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_24_141412) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_02_104047) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_141412) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "authorizations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", unique: true
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -70,5 +80,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_24_141412) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "messages", "users"
 end
