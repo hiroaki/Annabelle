f3839d364b3ad7a595fcc4f74d1eed168b5fe62a
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-05-09 17:11:59 +0900

    adding :otp_attempt as permitted parameter in Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
