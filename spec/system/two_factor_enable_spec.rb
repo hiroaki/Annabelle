5ad045929f23c3ad06513dc9b1a65d27f5e170a6
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-05-29 20:20:58 +0900

    improve system spec for two_factor_enable_spec by using testid

--- spec/system/two_factor_enable_spec.rb
+++ spec/system/two_factor_enable_spec.rb
@@ -16,19 +16,19 @@ RSpec.describe '2FA有効化フロー', type: :system do
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: password
-    click_button 'Log in'
+    find('[data-testid="login-button"]').click
 
     # 右上のユーザ名クリック
-    find_link('current-user-display').click
+    find('[data-testid="current-user-display"]').click
 
     # 左のメニューをクリック
-    click_on 'Two Factor Authentication'
+    find('[data-testid="configuration-menu-two-factor-authentication"]').click
 
     # 右のページ、 Enableボタン押下
-    click_on('users_two_factor_authentication_enable')
+    find('[data-testid="users_two_factor_authentication_enable"]').click
 
     secret = nil
-    within('p', text: /please enter the following code manually/) do
+    within('p', text: /please enter the following code manually/i) do
       secret = find('code').text
     end
     totp = ROTP::TOTP.new(secret)
@@ -36,10 +36,10 @@ RSpec.describe '2FA有効化フロー', type: :system do
 
     fill_in 'Code', with: code
     fill_in 'Enter your current password', with: password
-    click_button 'Confirm and Enable Two Factor'
+    find('[data-testid="confirm-and-enable-two-factor"]').click
 
-    expect(page).to have_content('Two factor authentication has been successfully enabled')
-    expect(page).to have_content('Backup Codes')
+    expect(page).to have_selector('[data-testid="two-factor-enabled-message"]')
+    expect(page).to have_selector('[data-testid="backup-codes-title"]')
 
     codes = all_testid('backup-code').map { |elem| elem.text }.select { |text| text.match?(/\A[a-f0-9]{32}\z/i) }
     expect(codes.size).to eq(5)
