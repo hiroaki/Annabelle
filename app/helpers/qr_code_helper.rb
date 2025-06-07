1fb6ad7da8734fb045af5e385ee7cc3ea1725ef1
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-06-07 17:49:35 +0900

    refactor qr_code_as_svg method to ensure uri validation and update related tests

--- app/helpers/qr_code_helper.rb
+++ app/helpers/qr_code_helper.rb
@@ -1,11 +1,13 @@
 module QrCodeHelper
   def qr_code_as_svg(uri)
+    raise ArgumentError, 'uri must not be blank' if uri.blank?
+
     RQRCode::QRCode.new(uri).as_svg(
-        offset: 0,
-        color: '000',
-        shape_rendering: 'crispEdges',
-        module_size: 4,
-        standalone: true
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 4,
+      standalone: true
     ).html_safe
   end
 end
--- spec/helpers/qr_code_helper_spec.rb
+++ spec/helpers/qr_code_helper_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe QrCodeHelper, type: :helper do
       expect(svg).to include('crispEdges')
       expect(svg).to include('standalone')
     end
+
+    it 'nilを渡した場合はArgumentErrorになる' do
+      expect { helper.qr_code_as_svg(nil) }.to raise_error(ArgumentError)
+    end
+
+    it '空文字を渡した場合はArgumentErrorになる' do
+      expect { helper.qr_code_as_svg('') }.to raise_error(ArgumentError)
+    end
+
+    it '不正な値を渡した場合もエラーにならずSVGを返す' do
+      svg = helper.qr_code_as_svg('!!!')
+      expect(svg).to include('<svg')
+    end
   end
 end
