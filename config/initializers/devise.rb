7e06a78cad7da1863dba3e6dc80476709f3200c8
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-08-03 01:52:44 +0900

    Reduce GitHub OAuth scope to minimum required for login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,16 +277,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
-  # config.omniauth(:github,
-  #   Rails.application.credentials.github[:client_id],
-  #   Rails.application.credentials.github[:client_secret],
-  #   scope: 'user,public_repo'
-  # )
   config.omniauth(:github,
     ENV['GITHUB_CLIENT_ID'],
     ENV['GITHUB_CLIENT_SECRET'],
-    scope: 'user,public_repo'
+    scope: 'read:user,user:email'
   )
 
   # ==> Warden configuration
