97a1464a837f84d1baeb64cc5d4f32be47fe1fed
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-10-15 23:28:51 +0900

    add an experimantal feature - gpx overlayed map embedding

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -3,18 +3,67 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['preview', 'modal', 'modalBody']
 
+  _generateMapContainer(mapId) {
+    const content = document.createElement('div');
+    content.id = mapId;
+    content.style.width = "800px";
+    content.style.height = "800px";
+    content.className = "object-contain max-h-full max-w-full mx-auto my-2";
+    content.setAttribute('data-guard-closing-preview', 'true');
+    return content;
+  }
+
+  _activateMapContent(mapId, gpxUrl){
+    const map = L.map(mapId).setView([35.6812, 139.7671], 13); // 東京駅付近
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    new L.GPX(gpxUrl, {
+      async: true,
+      markers: {
+        startIcon: 'leaflet-gpx/icons/pin-icon-start.png',
+        endIcon: 'leaflet-gpx/icons/pin-icon-end.png',
+        wptIcons: {}
+      },
+      marker_options: {
+        iconSize: [24, 32],
+        iconAnchor: [12, 32],
+      }
+    }).on('loaded', function(e) {
+      map.fitBounds(e.target.getBounds());
+    }).addTo(map);
+  }
+
   changePreview(evt) {
     evt.preventDefault();
 
-    const content = evt.currentTarget.querySelector('img, video').cloneNode(true);
-
     if (this.isDisplayed(this.previewTarget)) {
       this.clearPreview();
-      this.previewTarget.appendChild(content);
+
+      // WIP:
+      if (evt.currentTarget.dataset["contentType"] != "application/xml" || !evt.currentTarget.dataset["filename"].match("\.gpx$") ) {
+        const content = evt.currentTarget.querySelector('img, video').cloneNode(true);
+        this.previewTarget.appendChild(content);
+      } else {
+        const content = this._generateMapContainer("map");
+        this.previewTarget.appendChild(content);
+        const map = this._activateMapContent(content.id, evt.currentTarget.dataset["sourceUrl"]);
+        map.invalidateSize();
+      }
     } else {
       this.clearModal();
-      this.modalBodyTarget.appendChild(content);
       this.openModal();
+      if (evt.currentTarget.dataset["contentType"] != "application/xml" || !evt.currentTarget.dataset["filename"].match("\.gpx$") ) {
+        const content = evt.currentTarget.querySelector('img, video').cloneNode(true);
+        this.modalBodyTarget.appendChild(content);
+      } else {
+        const content = this._generateMapContainer("map");
+        this.modalBodyTarget.appendChild(content);
+        const map = this._activateMapContent(content.id, evt.currentTarget.dataset["sourceUrl"]);
+        map.invalidateSize();
+      }
     }
   }
 
@@ -23,10 +72,32 @@ export default class extends Controller {
   }
 
   handlerClearPreview(evt) {
+    // WIP:
+    if (evt.currentTarget.dataset["guardClosingPreview"]) {
+      console.log("guard by event currentTarget");
+      return;
+    }
+    // WIP:
+    if (evt.target.dataset["guardClosingPreview"]) {
+      console.log("guard by event target");
+      return;
+    }
+
     this.clearPreview();
   }
 
   handlerCloseModal(evt) {
+    // WIP:
+    if (evt.currentTarget.dataset["guardClosingPreview"]) {
+      console.log("guard by event currentTarget");
+      return;
+    }
+    // WIP:
+    if (evt.target.dataset["guardClosingPreview"]) {
+      console.log("guard by event target");
+      return;
+    }
+
     this.closeModal();
   }
 
