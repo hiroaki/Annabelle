288e0513df53616e65184980472189a4ac91381a
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-12-13 13:20:12 +0900

    Remove Tailwind config plugins as they are no longer supported in v4

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,9 +18,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
 }
