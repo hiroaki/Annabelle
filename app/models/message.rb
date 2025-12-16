f09d622997b66bda7c290e59f23a86b3f336e683
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-12-16 21:08:32 +0900

    Implement automatic purge of attachments on Message destruction and add corresponding specs

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
-  has_many_attached :attachments
+  has_many_attached :attachments, dependent: :purge_later
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -46,4 +46,39 @@ RSpec.describe Message, type: :model do
       expect(extracted['camera']).to eq({ 'make' => 'ExampleCam', 'model' => 'Imaginary 1' })
     end
   end
+
+  describe "削除時の添付ファイル自動削除" do
+    it "Message を削除すると添付ファイルの purge ジョブが実行される" do
+      Prosopite.pause do
+        user = create(:user)
+        message = create(:message, user: user)
+
+        blob1 = ActiveStorage::Blob.create_and_upload!(
+          io: StringIO.new('file-1'),
+          filename: 'file1.txt',
+          content_type: 'text/plain'
+        )
+        blob2 = ActiveStorage::Blob.create_and_upload!(
+          io: StringIO.new('file-2'),
+          filename: 'file2.txt',
+          content_type: 'text/plain'
+        )
+
+        message.attachments.attach([blob1, blob2])
+
+        # enqueued jobs をクリアして検証を厳密化
+        ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+
+        expect {
+          message.destroy
+        }.to have_performed_job(ActiveStorage::PurgeJob).exactly(2).times
+
+        # DB 側の関連レコード（ActiveStorage::Attachment）が消えていることも確認
+        expect(ActiveStorage::Attachment.where(record: message).exists?).to be false
+
+        # Blob 自体も（他レコードで共有されていない前提で）消えることを確認
+        expect(ActiveStorage::Blob.where(id: [blob1.id, blob2.id]).exists?).to be false
+      end
+    end
+  end
 end
